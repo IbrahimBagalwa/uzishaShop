ce00a9e2700aa6f8d937a8187c0a090cf6ee169f
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-07-05 15:15:45 +0200

    model user

---
+++ src/models/userModel.js
@@ -0,0 +1,41 @@
+import mongoose from "../cnx/database";
+
+const userSchema = mongoose.Schema({
+    created:{
+        type: Date,
+        default:Date.now(),
+        required: true
+    },
+    modified:{
+        type:Date,
+        default:Date.now(),
+        required:true
+    },
+    deleted:{
+        type:Date,
+        default:Date.now(),
+        required:false
+    },
+    nom:{
+        type:String,
+        required:true
+    },
+    prenom:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    photo:{
+        type:String,
+        required:true
+    }
+
+})
+module.exports = mongoose.model('users',userSchema)
