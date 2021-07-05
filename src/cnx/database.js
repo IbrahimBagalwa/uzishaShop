bfdc0a9b093ece0fbe76319b0bf8cca672cbb980
IbrahimBagalwa <bagmurhulaibrahim@gmail.com>
2021-07-05 14:57:50 +0200

    modif connection

--- src/cnx/database.js
+++ src/cnx/database.js
@@ -1,12 +1,16 @@
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
-const uri = process.env.MONGO_URI;
+const uri = process.env.MONGO_URI
+
 mongoose.connect(uri,{
     useNewUrlParser:true,
     useUnifiedTopology:true, 
     useCreateIndex:true
+},()=>{
+    console.log('Database Successfully Connected');    
 })
+
+
 export default mongoose
--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
-
+import cors from 'cors';
+import db from './cnx/database.js'
 
 dotenv.config();
+
 const app = express();
+
+app.use(cors());
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
+
+app.use('**', (req,res,next)=>{
+    res.status(200).send({status:405, msg:"Request not found on the server"})
+})
 
 
 const port = process.env.PORT | 8000;
